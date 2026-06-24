a818c19997d389835e6dc341d8b7b041da156df4
ibrahimaagg0-dev <ibrahimaagg0@gmail.com>
2026-06-24 16:39:46 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,29 @@
-﻿const cacheName = 'zaad-v1';
+const cacheName = 'zaad-v4';
 const assets = [
   './',
   './index.html',
   './style.css',
-  './manifest.json'
+  './manifest.json',
+  './icon.png',
+  './sabah.html',
+  './masa.html',
+  './quran.html'
 ];
 
+// حدث التثبيت وتخزين الملفات أوفلاين
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
+// حدث جلب البيانات وتشغيل الموقع بدون نت بذكاء
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
